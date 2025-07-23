57ec9b7a3aec5c5ce1072fc3d4e1d9bbba19c8ce
chu quang tu <chuquangtus2004@gmail.com>
2025-07-23 17:09:22 +0700

    Config_Mui_Theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import Button from '@mui/material/Button'
 import AccessAlarmIcon from '@mui/icons-material/AccessAlarm'
 import ThreeDRotation from '@mui/icons-material/ThreeDRotation'
+import Typography from '@mui/material/Typography'
 
 function App() {
 
   return (
     <div>
+      <Typography variant='body2' color='text.chuquangtu'>Step 3. Update the necessary typings (if you are using TypeScript)</Typography>
       <div>Chu quang tú</div>
-      <Button variant='contained' color="secondary">Hello world</Button>
+      <Button variant='main'>Hello world</Button>
       <AccessAlarmIcon/>
       <ThreeDRotation/>
     </div>
