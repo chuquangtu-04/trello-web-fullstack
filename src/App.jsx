9c799a83e1aa5369fb62692181fe4bff937c6a5b
chu quang tu <chuquangtus2004@gmail.com>
2025-07-25 01:05:24 +0700

    dark_light_p1

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,32 @@ import AccessAlarmIcon from '@mui/icons-material/AccessAlarm'
 import ThreeDRotation from '@mui/icons-material/ThreeDRotation'
 import Typography from '@mui/material/Typography'
 
+
+import { useColorScheme } from '@mui/material/styles'
+
+function ModeToggle() {
+  const { mode, setMode } = useColorScheme()
+  return (
+    <Button
+      onClick={() => {
+        setMode(mode === 'light' ? 'dark' : 'light')
+      }}
+    >
+      {mode === 'light' ? 'Turn dark' : 'Turn light'}
+    </Button>
+  )
+}
 function App() {
 
   return (
-    <div>
+    <>
+      <ModeToggle />
       <Typography variant='body2' color='text.chuquangtu'>Step 3. Update the necessary typings (if you are using TypeScript)</Typography>
       <div>Chu quang tú</div>
       <Button variant='main'>Hello world</Button>
       <AccessAlarmIcon/>
       <ThreeDRotation/>
-    </div>
+    </>
   )
 }
 
