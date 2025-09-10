9ba797ff583dbbb7b8b15b405e0623755eb288f2
chu quang tu <chuquangtus2004@gmail.com>
2025-09-10 22:20:40 +0700

    Update+++

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,10 +1,8 @@
 let apiRoot = ''
-console.log('process.env: ', process.env)
 if (process.env.BUILD_MODE === 'dev') {
   apiRoot = 'http://localhost:8017'
 }
 if (process.env.BUILD_MODE === 'production') {
   apiRoot = 'https://trello-web-api-qh07.onrender.com'
 }
-console.log('🚀 ~ apiRoot:', apiRoot)
 export const API_ROOT = apiRoot
