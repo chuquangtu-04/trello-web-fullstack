ff50f69e4e7df55ee90965e59691c904dd471f6e
chu quang tu <chuquangtus2004@gmail.com>
2025-09-11 08:13:50 +0700

    Confix Redux

--- src/main.jsx
+++ src/main.jsx
@@ -9,23 +9,26 @@ import theme from '~/theme.js'
 // Custom dialog Mui
 import { ConfirmProvider } from 'material-ui-confirm'
 
+//Cấu hình Redux Store
+import { Provider } from 'react-redux'
+import { store } from '~/redux/store'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  // <React.StrictMode>
-  <CssVarsProvider theme={theme}>
-    {/* Reset toàn bộ các giá trị mặc định không nhất quán đó về một tiêu chuẩn chung. */}
-    <ConfirmProvider defaultOptions={{
-      allowClose: false,
-      dialogProps: { maxWidth: 'xs' },
-      cancellationButtonProps: { color: 'inherit' },
-      confirmationButtonProps: { color: 'secondary', variant: 'outlined' }
-    }}>
-      <CssBaseline />
-      <App />
-      <ToastContainer theme="colored"
-        position="bottom-left"
-      />
-    </ConfirmProvider>
-  </CssVarsProvider>
-  // </React.StrictMode>
+  <Provider store={store}>
+    <CssVarsProvider theme={theme}>
+      {/* Reset toàn bộ các giá trị mặc định không nhất quán đó về một tiêu chuẩn chung. */}
+      <ConfirmProvider defaultOptions={{
+        allowClose: false,
+        dialogProps: { maxWidth: 'xs' },
+        cancellationButtonProps: { color: 'inherit' },
+        confirmationButtonProps: { color: 'secondary', variant: 'outlined' }
+      }}>
+        <CssBaseline />
+        <App />
+        <ToastContainer theme="colored"
+          position="bottom-left"
+        />
+      </ConfirmProvider>
+    </CssVarsProvider>
+  </Provider>
 )
